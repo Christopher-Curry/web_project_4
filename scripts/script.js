74af4224bc759f4899090d129914747111fa6d17
Chris Curry <thelifeofcurry@gmail.com>
2021-01-09 15:38:29 -0800

    Submission fixes

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+
+let form = document.querySelector(".form")
+let name = document.querySelector(".profile__title")
+let input = document.querySelector("#fullName")
+let description = document.querySelector(".profile__subtitle")
+let descriptionInput = document.querySelector("#description")
+
+function showForm() {
+    form.classList.add("form_active")
+    input.value = name.textContent
+    descriptionInput.value = description.textContent
+}
+
+function closeForm() {
+    form.classList.remove("form_active")
+}
+
+function saveButton() {
+    name.textContent = input.value
+    description.textContent = descriptionInput.value
+    form.classList.remove("form_active")
+    return false;
+}
