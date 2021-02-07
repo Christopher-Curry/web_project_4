217d90c2de12a63aac79a8ebab175ec94b691163
Chris Curry <thelifeofcurry@gmail.com>
2021-02-06 20:29:23 -0800

    enable validation

--- scripts/validate.js
+++ scripts/validate.js
@@ -17,26 +17,50 @@ const isValid = (element, classNameForSpan) => {
   }
 };
 
-profilePopupName.addEventListener("submit", function (evt) {
-  evt.preventDefault();
+function toggleButtonState(form, buttonElement, inactiveButtonClass) {
+  if (!form.checkValidity()) {
+    buttonElement.classList.add(inactiveButtonClass);
+  } else {
+    buttonElement.classList.remove(inactiveButtonClass);
+  }
+}
+
+toggleButtonState(newCardForm, newCardCreateButton);
+
+function enableValidation({
+  formSelector,
+  inputSelector,
+  submitButtonSelector,
+  inactiveButtonClass,
+  inputErrorClass,
+  errorClass,
+}) {
+  let form = document.querySelector(formSelector);
+  let submitButton = document.querySelector(submitButtonSelector);
+  let allInputs = document.querySelectorAll(inputSelector);
+
+  allInputs.forEach((element) => {
+    element.addEventListener("input", function () {
+      isValid(element, errorClass);
+      toggleButtonState(form, submitButton, inactiveButtonClass);
+    });
+  });
+}
+
+enableValidation({
+  formSelector: ".form__container",
+  inputSelector: ".form__input",
+  submitButtonSelector: ".form__save-btn",
+  inactiveButtonClass: "profile-popup__create-btn_disabled",
+  inputErrorClass: ".form__error",
+  errorClass: "div-error_inactive",
 });
 
-profilePopupName.addEventListener("input", function () {
-  isValid(profilePopupName, "div-error_inactive");
-  toggleButtonState(profilePopupForm, profilePopupCreateButton);
-});
-
-profilePopupDescription.addEventListener("input", function () {
-  isValid(profilePopupDescription, "div-error_inactive");
-  toggleButtonState(profilePopupForm, profilePopupCreateButton);
-});
-
-newCardTitle.addEventListener("input", function () {
-  isValid(newCardTitle, "div-error_inactive");
-  toggleButtonState(newCardForm, newCardCreateButton);
-});
-
-newCardLink.addEventListener("input", function () {
-  isValid(newCardLink, "div-error_inactive");
-  toggleButtonState(newCardForm, newCardCreateButton);
+enableValidation({
+  formSelector: ".popup__container",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__create-btn",
+  inactiveButtonClass: "profile-popup__create-btn_disabled",
+  inputErrorClass: "profile-popup__error",
+  errorClass: "div-error_inactive",
 });
