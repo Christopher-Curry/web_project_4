79f78d11521e55ef5e729db04894bed748e42f5b
Chris Curry <thelifeofcurry@gmail.com>
2021-02-06 19:56:27 -0800

    Updating BEM

--- scripts/validate.js
+++ scripts/validate.js
@@ -22,21 +22,21 @@ profilePopupName.addEventListener("submit", function (evt) {
 });
 
 profilePopupName.addEventListener("input", function () {
-  isValid(profilePopupName, "form__div-error_inactive");
+  isValid(profilePopupName, "div-error_inactive");
   toggleButtonState(profilePopupForm, profilePopupCreateButton);
 });
 
 profilePopupDescription.addEventListener("input", function () {
-  isValid(profilePopupDescription, "form__div-error_inactive");
+  isValid(profilePopupDescription, "div-error_inactive");
   toggleButtonState(profilePopupForm, profilePopupCreateButton);
 });
 
 newCardTitle.addEventListener("input", function () {
-  isValid(newCardTitle, "form__div-error_inactive");
+  isValid(newCardTitle, "div-error_inactive");
   toggleButtonState(newCardForm, newCardCreateButton);
 });
 
 newCardLink.addEventListener("input", function () {
-  isValid(newCardLink, "form__div-error_inactive");
+  isValid(newCardLink, "div-error_inactive");
   toggleButtonState(newCardForm, newCardCreateButton);
 });
