e7fcbfed37fb406b70f612589498c8737ce88d61
Chris Curry <thelifeofcurry@gmail.com>
2021-03-07 16:16:02 -0800

    Fixed Popup With Image

--- src/scripts/PopupWithImage.js
+++ src/scripts/PopupWithImage.js
@@ -1,16 +1,18 @@
 import Popup from "./Popup.js";
 
 class PopupWithImage extends Popup {
-  constructor(popupSelector) {
+  constructor(popupSelector, imageSelector, titleSelector) {
     super(popupSelector);
+    this._imageSelector = imageSelector;
+    this._titleSelector = titleSelector;
   }
   open(src, textContent) {
     super.open();
     console.log(src, textContent);
-    let image = this.popup.querySelector(".image__popup");
+    let image = this.popup.querySelector(this._imageSelector);
     image.src = src;
 
-    let title = this.popup.querySelector(".image__title");
+    let title = this.popup.querySelector(this._titleSelector);
     title.textContent = textContent;
   }
 }
